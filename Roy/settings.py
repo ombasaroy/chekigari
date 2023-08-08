a4a6c162f8d6e88459342d940cd53401b10e1f85
ombasaroy <ombasaroy@gmail.com>
2023-08-08 19:22:17 +0300

    Updated records

--- Roy/settings.py
+++ Roy/settings.py
@@ -37,10 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'my_app',
-    'second_app',
     'chekigari',
-    'crud'
 ]
 
 MIDDLEWARE = [
--- Roy/urls.py
+++ Roy/urls.py
@@ -7,5 +7,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chekigari.urls')),
-    path('myapp/', include("my_app.urls"))
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
